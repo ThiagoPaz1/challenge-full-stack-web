87c975bd7917b9faa7e538a48634442306661a92
Thiago da Paz <thiagopazjunior@gmail.com>
2022-08-05 13:46:46 -0300

    fix: Ajustes.

--- api/src/app.js
+++ api/src/app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const dotenv = require('dotenv');
 var cors = require('cors')
 const app = express();
- 
+const studentRouter = require('./routers/index');
+
 dotenv.config();
 
 app.use(cors())
 app.use(express.json())
-// app.use('/student');
+app.use('/student', studentRouter);
 
 app.listen(process.env.PORT, () => {
   console.log('Serve is running')
