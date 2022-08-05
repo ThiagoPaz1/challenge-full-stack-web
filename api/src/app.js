17fcb914e8934d2e6e55cab2439bcbc41d4b42be
Thiago da Paz <thiagopazjunior@gmail.com>
2022-08-05 13:43:11 -0300

    feat: Adicionado lib cors.

--- api/src/app.js
+++ api/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
+var cors = require('cors')
 const app = express();
-
+ 
 dotenv.config();
 
+app.use(cors())
 app.use(express.json())
 // app.use('/student');
 
