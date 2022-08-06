9bfd5eb71700ed63d99eae3f8f06341e013ed288
Thiago da Paz <thiagopazjunior@gmail.com>
2022-08-06 11:33:45 -0300

    feat: Criado service findOne, em studentService.

--- api/src/controllers/studentController.js
+++ api/src/controllers/studentController.js
@@ -1,7 +1,18 @@
 const studentService = require('../services/studentService');
 
 const newStudent = async (req, res) => {
-  
+  const { name, email, cpf } = req.body;
+  const fields = [ name, email, cpf ];
+  const teste = await studentService.findOne({where: {cpf: cpf}})
+  console.log(teste)
+  for (let i in fields) {
+    if (!fields[i].length) {
+      return res.status(400).send('Todos os campos devem ser preenchidos');
+    }
+  }
+
+  if (cpf.length < 11) return res.status(400).send('CPF deve conter 11 digitos.');
+
   try {
     await studentService.create({name, email, cpf});
 
--- api/src/services/studentService.js
+++ api/src/services/studentService.js
@@ -6,6 +6,11 @@ const create = async (body) => {
   return await Student.create({name, email, ru: '15345', cpf});
 }
 
+const findOne = async (search) => {
+ return await Student.findOne(search)
+}
+
 module.exports = {
   create,
+  findOne,
 }
