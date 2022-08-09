cdd19437a47cf6a7da7ded51cc70c5bc9e4ca6a1
Thiago da Paz <thiagopazjunior@gmail.com>
2022-08-08 23:47:50 -0300

    feat: Criado componente RegistrationForm.

---
+++ frontend/src/components/RegistrationForm.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  >
+    <v-text-field
+      v-model="name"
+      :counter="10"
+      :rules="nameRules"
+      label="Name"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="email"
+      :rules="emailRules"
+      label="E-mail"
+      required
+    ></v-text-field>
+
+  <v-text-field
+      placeholder="Digite sem os ' . ' e ' - ', ex: 00011122233"
+      v-model="cpf"
+      :rules="cpfRules"
+      label="CPF"
+      required
+    ></v-text-field>
+
+    <v-btn
+      :disabled="!valid"
+      color="success"
+      class="mr-4"
+      @click="validate"
+    >
+      Salvar
+    </v-btn>
+
+    <v-btn
+      color="error"
+      class="mr-4"
+    >
+      Cancelar
+    </v-btn>
+  </v-form>
+</template>
+
+<script>
+  export default {
+    name: 'RegistrationForm',
+    data: () => ({
+      valid: true,
+      name: '',
+      nameRules: [
+        v => !!v || 'Nome é obrigatório',
+        v => v.length == 3 || 'Nome precisa ter no mínimo 3 caracteres',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail é obrigatório',
+        v => /.+@.+\..+/.test(v) || 'Insira um e-mail valido',
+      ],
+      cpf: '',
+      cpfRules: [
+        v => !!v || 'CPF é obrigatório',
+        v => v.length == 11 || 'CPF precisa ter 11 digitos',  
+      ],
+    }),
+
+    methods: {
+      validate() {
+        this.$refs.form.validate()
+      },
+    },
+  }
+</script>
--- frontend/src/routers/index.js
+++ frontend/src/routers/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import PageStudent from '../pages/student/PageStudent'
+import PageStudent from '../pages/student/PageStudent';
+import PageRegistrationStudent from '../pages/student/PageRegistrationStudent';
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/students',
     name: 'pageStudent',
     component: PageStudent,
+  },
+  {
+    path: '/registrationStudents',
+    name: 'pageRegistrationStudent',
+    component: PageRegistrationStudent,
   }
 ];
 
